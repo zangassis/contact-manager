199bfe07b3832df9c21d02bb944cdff795043655
Assis Zang <assis.compasso@dotzmkt.com.br>
2022-12-08 15:37:30 -0300

    feat: Created the first version

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
+
+const app = express();
+const PORT = 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://0.0.0.0:27017/CRMdb', {
+    useNewUrlParser: true
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serving static files
+app.use(express.static('public'));
+
+routes(app);
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`Your server is running on port ${PORT}`)
+);
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,18 @@
+import { addNewContact, getContacts, getContactById, updateContact, deleteContact } from "../controllers/crmController"
+
+const routes = (app) => {
+    app.route('/contact')
+        .get((req, res, next) => {
+            console.log(`Request from: ${req.originalUrl}`)
+            console.log(`Request type: ${req.method}`)
+            next();
+        }, getContacts)
+        .post(addNewContact);
+
+    app.route('/contact/:contactId')
+        .get(getContactById)
+        .put(updateContact)
+        .delete(deleteContact)
+}
+
+export default routes;
